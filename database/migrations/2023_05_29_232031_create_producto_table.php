fd424c6d9d85aeaa96df5a0328c1111d653d1ae8
Hiroshito22 <hgnizama2004@gmail.com>
2023-05-29 23:56:55 -0500

    gaaaaaaaaaaa

---
+++ database/migrations/2023_05_29_232031_create_producto_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('producto', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo')->nullable();
+            $table->string('nombre')->nullable();
+            $table->string('categoria')->nullable();
+            $table->string('proveedor')->nullable();
+            $table->integer('cantidad')->nullable();
+            $table->string('precio')->nullable();
+            $table->char('estado_registro')->default('A');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('producto');
+    }
+}
